76d2438401434bb610dadf7b7e82b7f2d0981cdb
patweatharva <patweatharva@gmail.com>
2025-07-09 16:19:58 +0200

    added mappings changed options changed theme

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,51 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Custom Remaps
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+map("n", "<leader>vwm", function()
+    require("vim-with-me").StartVimWithMe()
+end)
+map("n", "<leader>svwm", function()
+    require("vim-with-me").StopVimWithMe()
+end)
+
+-- greatest remap ever
+map("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+-- map({"n", "v"}, "<leader>y", [["+y]])
+-- map("n", "<leader>Y", [["+Y]])
+
+map({ "n", "v" }, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+map("i", "<C-c>", "<Esc>")
+
+map("n", "Q", "<nop>")
+-- map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+map("n", "<leader>f", vim.lsp.buf.format)
+
+map("n", "<C-k>", "<cmd>cnext<CR>zz")
+map("n", "<C-j>", "<cmd>cprev<CR>zz")
+map("n", "<leader>k", "<cmd>lnext<CR>zz")
+map("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+map("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+map("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+map("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
