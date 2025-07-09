3020353c32dbda739b1abe38f367918c7e0e8668
patweatharva <patweatharva@gmail.com>
2025-07-10 00:25:43 +0200

    configured lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,32 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+local lspconfig = require("lspconfig")
+
+-- Language servers to set up
+local servers = {
+  "bashls",              -- bash-language-server
+  "clangd",              -- C/C++
+  "cmake",               -- cmake-language-server
+  "coffeesense",         -- coffeesense-language-server
+  "cssls",               -- css-lsp
+  "docker_compose_language_service", -- docker-compose
+  "dockerls",            -- dockerfile-language-server
+  "html",                -- html-lsp
+  "lua_ls",              -- lua-language-server
+  "marksman",            -- markdown
+  "pyright",             -- Python
+  "ruff",            -- Python linter
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
